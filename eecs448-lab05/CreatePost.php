17a41e5e09115a0d767186bb48ef75c9d71e16c1
czfloyd <floyd.c23@gmail.com>
2019-04-02 00:19:03 -0500

    Added post creation portal

---
+++ eecs448-lab05/CreatePost.php
@@ -0,0 +1,27 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "c827f144", "eiphah4k", "c827f144");
+
+$user = $_POST["user"];
+$content = $_POST["content"];
+/* check connection */
+if ($mysqli->connect_errno) {
+    echo("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$query = "INSERT INTO Posts (content, author_id) VALUES ('" . $content . "', '" . $user . "')";
+
+if ($result = $mysqli->query($query)) {
+
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        echo("Message posted successfully!");
+    }
+
+    /* free result set */
+    $result->free();
+}
+
+/* close connection */
+$mysqli->close();
+?>
