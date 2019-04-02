f04a275af12453973114f26ae9f356206465707c
czfloyd <floyd.c23@gmail.com>
2019-04-01 20:13:14 -0500

    Fixed print statements for create user

--- eecs448-lab05/CreateUser.php
+++ eecs448-lab05/CreateUser.php
@@ -4,7 +4,7 @@ $mysqli = new mysqli("mysql.eecs.ku.edu", "c827f144", "eiphah4k", "c827f144");
 $user = $_POST["user"];
 /* check connection */
 if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
+    echo("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
 
@@ -14,7 +14,7 @@ if ($result = $mysqli->query($query)) {
 
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
-        printf ("User added successfully");
+        echo("User added successfully");
     }
 
     /* free result set */
