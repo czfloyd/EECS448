37753e82b668058e7d42e3d077ed568b2e4c16f0
czfloyd <floyd.c23@gmail.com>
2019-04-04 01:11:25 -0500

    Displayed post_ids of deleted posts

--- eecs448-lab05/DeletePost.php
+++ eecs448-lab05/DeletePost.php
@@ -1,6 +1,6 @@
 <?php
 $mysqli = new mysqli("mysql.eecs.ku.edu", "c827f144", "eiphah4k", "c827f144");
-echo '<html><head><title>User List</title><link href="../styles/style.css" rel="stylesheet" type="text/css" /></head>';
+echo '<html><head><title>Post Deletion Landing</title><link href="../styles/style.css" rel="stylesheet" type="text/css" /></head>';
 echo '<body>';
 
 $postsToDelete = $_POST["postsToDelete"];
@@ -10,14 +10,19 @@ if ($mysqli->connect_errno) {
     exit();
 }
 
+echo "The Following posts: <br/>";
 $query = "DELETE FROM Posts WHERE post_id IN (";
 foreach ($postsToDelete as $postsToDelete=>$post){
     $query = $query . "" . $post . ", ";
+    echo $post . "<br/>";
 }
 $query = substr($query, 0, -2) . ")";
 
 if ($result = $mysqli->query($query)) {
-    echo("Posts deleted successfully");
+    echo "were deleted successfully.";
+}
+else {
+    echo "failes to delete.";
 }
 
 echo '</body>';
